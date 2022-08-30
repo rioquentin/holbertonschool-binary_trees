1a49708b242e4babc0a1e7d57323499675f33d72
quentin <quentin.rio.pro@gmail.com>
2022-08-30 05:20:10 -0700

    maloc problem

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,12 +12,20 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 	
+	if (node == NULL);
+		return (NULL);
+
 	if (parent == NULL)
 	{
 		node->parent = parent;
 		node->left = NULL;
 		node->right = NULL;
 		node->n = value;
+		return (node);
 	}
+	node->parent = parent;
+	node->left = NULL;
+	node->right =NULL;
+	node->n = value;
 	return (node);
 }
