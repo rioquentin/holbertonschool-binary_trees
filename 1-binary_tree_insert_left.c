4ab3cb44cdfe3f51ac53e542d44725455a38ecef
quentin <quentin.rio.pro@gmail.com>
2022-08-30 06:38:32 -0700

    fixed malloc return

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
+	if (node == NULL)
+		return (NULL);
+
 	if (parent == NULL)
 	{
 		return(binary_tree_node(parent, value));
