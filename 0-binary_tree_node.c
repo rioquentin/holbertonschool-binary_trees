5aac2e225b3ee908283bc7ebb881c62885f8097b
quentin <quentin.rio.pro@gmail.com>
2022-08-30 04:51:55 -0700

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - func to create a binary tree
+ *
+ * @parent: parent of the node we want to create
+ * @value: value of the node
+ * Return: a pointer to the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	
+	if (parent == NULL)
+	{
+		node->parent = parent;
+		node->left = NULL;
+		node->right = NULL;
+		node->n = value;
+	}
+	return (node);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# holbertonschool-binary_trees
+READ
