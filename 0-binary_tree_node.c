92290dc3951479237bf9c3a29fdd2dc6bfc20339
quentin <quentin.rio.pro@gmail.com>
2022-08-30 05:22:19 -0700

    Segfault fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,8 +11,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-	
-	if (node == NULL);
+
+	if (node == NULL)
 		return (NULL);
 
 	if (parent == NULL)
@@ -25,7 +25,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	}
 	node->parent = parent;
 	node->left = NULL;
-	node->right =NULL;
+	node->right = NULL;
 	node->n = value;
 	return (node);
 }
