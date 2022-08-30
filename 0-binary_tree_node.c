f78675df9f4170e8aa0d708df2cddfbc27f91fa6
quentin <quentin.rio.pro@gmail.com>
2022-08-30 07:00:44 -0700

    fixed memory

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,14 +15,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (node == NULL)
 		return (NULL);
 
-	if (parent == NULL)
-	{
-		node->parent = parent;
-		node->left = NULL;
-		node->right = NULL;
-		node->n = value;
-		return (node);
-	}
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,15 +10,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node; 
+
+	node = binary_tree_node(parent, value);
 
 	if (node == NULL)
 		return (NULL);
-
-	if (parent == NULL)
-	{
-		return(binary_tree_node(parent, value));
-	}
 
 	node->n = value;
 
