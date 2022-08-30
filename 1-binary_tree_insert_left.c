3332e14283f3964ab42084d30e3f7f81d2a5ed6e
quentin <quentin.rio.pro@gmail.com>
2022-08-30 07:13:29 -0700

    fixed memory

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,8 +16,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (node == NULL)
 		return (NULL);
-
-	node->n = value;
+	if (parent == NULL)
+		return (node);
 
 	if (parent->left == NULL)
 	{
