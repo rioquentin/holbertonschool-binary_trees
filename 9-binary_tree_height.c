0fcf3f7cfbc846cd9b28d3d988a12778f8688624
quentin <quentin.rio.pro@gmail.com>
2022-08-31 06:00:28 -0700

    = changed in +=

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,8 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	else
 	{
-		lh = binary_tree_height(tree->left);
-		rh = binary_tree_height(tree->right);
+		lh += binary_tree_height(tree->left);
+		rh += binary_tree_height(tree->right);
 		if (lh >= rh)
 			return (lh + 1);
 		else
