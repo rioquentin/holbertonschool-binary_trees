cd99d10c2d33583afac2e709b7fc173772fbd956
quentin <quentin.rio.pro@gmail.com>
2022-08-31 00:39:14 -0700

    alloc/free fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,12 +12,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent == NULL)
+		return (NULL);
 	node = binary_tree_node(parent, value);
 
 	if (node == NULL)
 		return (NULL);
-	if (parent == NULL)
-		return (node);
 
 	if (parent->left == NULL)
 	{
