c7b33e13177e90821e67f4dea6da97f1a690a9a0
quentin <quentin.rio.pro@gmail.com>
2022-08-31 03:18:54 -0700

    fixed missing condition

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_preorder - func to print in preorder
+ * @tree: the binary tree
+ * @func: a func
+ * Return: void
+ */
+
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL)
+		return;
+	if (func == NULL)
 		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
