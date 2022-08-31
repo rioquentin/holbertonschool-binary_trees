f68556805f1b900f0e61c5f260a53c1e14544419
quentin <quentin.rio.pro@gmail.com>
2022-08-31 05:39:04 -0700

    find height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * 9-binary_tree_height - func to find height
+ *
+ * @tree: binary tree
+ * Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int lh, rh;
+
+	if (tree == NULL)
+		return (0);
+
+	else
+	{
+		lh = binary_tree_height(tree->left);
+		rh = binary_tree_height(tree->right);
+		if (lh >= rh)
+			return (lh + 1);
+		else
+			return (rh + 1);
+	}
+}
