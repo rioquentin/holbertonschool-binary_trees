dbe67e0b1a95fbd20577fb58889d40076ea38054
quentin <quentin.rio.pro@gmail.com>
2022-09-01 04:31:32 -0700

    count number of node with childs

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - func to count nodes with shield
+ *
+ * @tree: binary tree
+ * Return: number of nodes with child
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int num;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left)
+		num = binary_tree_leaves(tree->left);
+	if (tree->right)
+		num = binary_tree_leaves(tree->right);
+
+	if (!tree->left && !tree->right)
+		return (num);
+	return (num + 1);
+}
