21a07f625e1dcac77d7ecb7904cbeb8ff3d02a2e
quentin <quentin.rio.pro@gmail.com>
2022-09-01 05:54:31 -0700

    check uncle's node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -27,5 +27,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (!node)
+		return (NULL);
 	return (binary_tree_sibling(node->parent));
 }
