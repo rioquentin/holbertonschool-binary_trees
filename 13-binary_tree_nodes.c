820de4d4047c532ea8cb00e286a943e55cdfe730
quentin <quentin.rio.pro@gmail.com>
2022-09-01 04:32:56 -0700

    fixed wrong func name

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,9 +14,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		num = binary_tree_leaves(tree->left);
+		num = binary_tree_nodes(tree->left);
 	if (tree->right)
-		num = binary_tree_leaves(tree->right);
+		num = binary_tree_nodes(tree->right);
 
 	if (!tree->left && !tree->right)
 		return (num);
