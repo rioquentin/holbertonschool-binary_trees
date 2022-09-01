d291aff88c935bf64eb8fe3ff7b723237d203d7c
quentin <quentin.rio.pro@gmail.com>
2022-09-01 06:08:13 -0700

    14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - func to find height
+ *
+ * @tree: binary tree
+ * Return: height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int lh = 0, rh = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+	{
+		lh = binary_tree_height(tree->left);
+		lh++;
+	}
+	if (tree->right)
+	{
+		rh += binary_tree_height(tree->right);
+		rh++;
+	}
+
+	if (lh >= rh)
+		return (lh);
+	else
+		return (rh);
+}
+
+/**
+ * binary_tree_balance - func to get balance factor
+ *
+ * @tree: btree
+ * Return: balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int lh = 0, rh = 0;
+
+	if (!tree)
+		return (0);
+
+	lh = binary_tree_height(tree->left);
+	rh = binary_tree_height(tree->right);
+
+	return (lh - rh);
+}
