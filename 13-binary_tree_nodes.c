047476054f20a8c2a41b80dc2cde09f0985df31b
quentin <quentin.rio.pro@gmail.com>
2022-09-01 05:19:37 -0700

    test

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,11 +14,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		num = binary_tree_nodes(tree->left);
+		num += binary_tree_nodes(tree->left);
 	if (tree->right)
-		num = binary_tree_nodes(tree->right);
+		num += binary_tree_nodes(tree->right);
 
-	if (!tree->left && !tree->right)
-		return (num);
-	return (num + 1);
+	if (tree->left || tree->right)
+		num++;
+	return (num);
 }
