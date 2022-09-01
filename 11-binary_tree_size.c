df766b433fced5d717b73b056b8fb7dffd3c4ac6
quentin <quentin.rio.pro@gmail.com>
2022-09-01 01:36:04 -0700

    func to mesure size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,27 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_size - mesure the size
+ *
+ * @tree: binary tree
+ * Return: size of btree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left && tree->right)
+		return (size);
+
+	if (tree->left)
+		size += binary_tree_size(tree->left);
+	if (tree->right)
+		size += binary_tree_size(tree->right);
+	size++;
+
+	return (size);
+}
