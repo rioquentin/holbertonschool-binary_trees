820d69f63a8012f498de337b0fbf8b4c6a0eadb4
Cygnus-8 <muchrismonfort@gmail.com>
2022-09-01 18:56:43 -0700

    Fixing betty and Root implementation bug

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,7 +9,8 @@
  * or NULL if not
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+ const binary_tree_t *second)
 {
 	binary_tree_t *dad_first, *dad_second;
 
@@ -23,8 +24,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 
 	if (first == dad_second || !dad_first || (!dad_first && dad_second))
 		return (binary_trees_ancestor(first, dad_second));
-	else if (second == dad_first || !dad_second || (!dad_second && dad_first))
+	if (second == dad_first || !dad_second || (!dad_second && dad_first))
 		return (binary_trees_ancestor(second, dad_first));
-	else
-		return (binary_trees_ancestor(dad_first, dad_second));
+	return (binary_trees_ancestor(dad_first, dad_second));
 }
