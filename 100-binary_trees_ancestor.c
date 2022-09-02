5410fcf8c6924a9ccb4eb1305427d4954275c353
Cygnus-8 <muchrismonfort@gmail.com>
2022-09-01 18:41:52 -0700

    Advanced 1 feature added

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -21,9 +21,9 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	dad_first = first->parent;
 	dad_second = second->parent;
 
-	if (first == dad_second || !dad_first)
+	if (first == dad_second || !dad_first || (!dad_first && dad_second))
 		return (binary_trees_ancestor(first, dad_second));
-	else if (second == dad_first || !dad_second)
+	else if (second == dad_first || !dad_second || (!dad_second && dad_first))
 		return (binary_trees_ancestor(second, dad_first));
 	else
 		return (binary_trees_ancestor(dad_first, dad_second));
